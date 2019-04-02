4542c96c215dac2047bb6324da096aededc3e9df
Kilari Teja <kilariteja9@gmail.com>
2019-04-03 00:19:21 +0530

    made_with_love: Add sample hook to start activity

--- app/src/main/java/xyz/ksdme/lingo/MainActivity.kt
+++ app/src/main/java/xyz/ksdme/lingo/MainActivity.kt
@@ -1,18 +1,25 @@
 package xyz.ksdme.lingo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import xyz.ksdme.lingo.services.QuestionOverlayService
 
 class MainActivity : AppCompatActivity() {
+    private val logTag = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        this.hooksInit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -25,5 +32,16 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun hooksInit() {
+        made_with_love.setOnClickListener {
+            Log.d(this.logTag, "launchService")
+            this.launchService()
+        }
+    }
+
+    private fun launchService() {
+        startService(Intent(this, QuestionOverlayService::class.java))
     }
 }
