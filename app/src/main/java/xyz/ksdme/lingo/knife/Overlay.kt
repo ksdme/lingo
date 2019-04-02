9bcc001fd7c67d7bf108423851128f660a6eb07e
Kilari Teja <kilariteja9@gmail.com>
2019-04-03 01:09:23 +0530

    knife/Overlay: Add Overlay permission utilities

---
+++ app/src/main/java/xyz/ksdme/lingo/knife/Overlay.kt
@@ -0,0 +1,33 @@
+package xyz.ksdme.lingo.knife
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.support.annotation.RequiresApi
+import android.support.v7.app.AppCompatActivity
+
+fun doesRequireOverlayAbility(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun hasOverlayAbility(context: Context): Boolean {
+    return Settings.canDrawOverlays(context)
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun requestOverlayAbility(activity: AppCompatActivity, requestCode: Int) {
+    val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+        Uri.parse("package:${activity.packageName}"))
+
+    activity.startActivityForResult(intent, requestCode)
+}
+
+fun safelyCheckIfHasOverlayAbility(context: Context): Boolean {
+    if (doesRequireOverlayAbility())
+        return hasOverlayAbility(context)
+
+    return true
+}
