d30be3a251a13765af086fd86f3a8f6d3bb0a384
Kilari Teja <kilariteja9@gmail.com>
2019-04-09 11:39:05 +0530

    MainActivity.kt: Inflate main words recycler view

--- app/src/main/java/xyz/ksdme/lingo/MainActivity.kt
+++ app/src/main/java/xyz/ksdme/lingo/MainActivity.kt
@@ -1,15 +1,18 @@
 package xyz.ksdme.lingo
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import xyz.ksdme.lingo.adapters.WordsAdapter
 import xyz.ksdme.lingo.knife.doesRequireOverlayAbility
 import xyz.ksdme.lingo.knife.requestOverlayAbility
 import xyz.ksdme.lingo.knife.safelyCheckIfHasOverlayAbility
@@ -18,6 +21,8 @@ import xyz.ksdme.lingo.services.QuestionOverlayService
 class MainActivity : AppCompatActivity() {
     private val logTag = "MainActivity"
     private val requestCodeOverlay = 9358
+
+    private lateinit var fontKarlaRegular: Typeface
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,11 @@ class MainActivity : AppCompatActivity() {
             this.initOverlayStuff()
         else if (doesRequireOverlayAbility())
             requestOverlayAbility(this, this.requestCodeOverlay)
+
+        this.fetch()
+        this.applyMakeUp()
+
+        this.setUpWordsRecycler()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -60,5 +70,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun launchService() {
         startService(Intent(this, QuestionOverlayService::class.java))
+    }
+
+    private fun fetch() {
+        ResourcesCompat.getFont(this, R.font.karla_regular)?.let { typeface ->
+            this.fontKarlaRegular = typeface
+        }
+    }
+
+    private fun applyMakeUp() {
+        this.fontKarlaRegular.let { typeface ->
+            global_switch.typeface = typeface
+        }
+    }
+
+    private fun setUpWordsRecycler() {
+        this.fontKarlaRegular?.let { typeface ->
+            words_recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            words_recycler.adapter = WordsAdapter(this, arrayListOf(
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World",
+                "Hello World"
+            ), typeface)
+        }
     }
 }
